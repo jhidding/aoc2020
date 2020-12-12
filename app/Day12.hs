34b6650b404df17baa7ecd4f7eae0d142b717346
Johan Hidding <j.hidding@esciencecenter.nl>
2020-12-12 08:31:53 +0100

    day 12; with lenses

--- app/Day12.hs
+++ app/Day12.hs
@@ -1,11 +1,21 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Day12 where
 
 import RIO hiding (try)
+-- import RIO.Lens
+import Lens.Micro.Platform
+
 import qualified RIO.Text as Text
 import RIO.State
 
-import Text.Megaparsec hiding (State)
-import Text.Megaparsec.Char
+import Text.Megaparsec
+    ( parse,
+      errorBundlePretty,
+      sepEndBy1,
+      Parsec,
+      MonadParsec(try),
+      ParseErrorBundle )
+import Text.Megaparsec.Char ( char, eol )
 import Text.Megaparsec.Char.Lexer (decimal)
 
 type Parser = Parsec Void Text
@@ -38,59 +48,66 @@ singleInstruction
     <|> try (turnRight   <$> (char 'R' >> decimal))
     <|> try (moveForward <$> (char 'F' >> decimal))
 
-data ManhattanRec = ManhattanRec
-    { mdDirection :: Int
-    , mdEastWest :: Int
-    , mdNorthSouth :: Int }
-
-newtype ManhattanDistance a = ManhattanDistance
-    { runMD :: State ManhattanRec a }
-    deriving (Functor, Applicative, Monad, MonadState ManhattanRec)
-
-instance Turtle ManhattanDistance where
-    goNorth     x = modify (\m@ManhattanRec{..} -> m { mdNorthSouth = mdNorthSouth + x })
-    goEast      x = modify (\m@ManhattanRec{..} -> m { mdEastWest   = mdEastWest + x })
-    turnLeft    x = modify (\m@ManhattanRec{..} -> m { mdDirection  = (mdDirection + x) `mod` 360 })
-    moveForward x = gets mdDirection >>= go
-            where go dir | dir == 0   = goEast x
-                         | dir == 90  = goNorth x
-                         | dir == 180 = goWest x
-                         | dir == 270 = goSouth x
-                         | otherwise  = return ()
-    getManhattanDistance = gets go
-        where go ManhattanRec{..} = abs mdEastWest + abs mdNorthSouth
-
-data WaypointRec = WaypointRec
-    { waypointX :: Int
-    , waypointY :: Int
-    , waypointEast :: Int
-    , waypointNorth :: Int }
-
-newtype Waypoint a = Waypoint
-    { runWaypoint :: State WaypointRec a }
-    deriving (Functor, Applicative, Monad, MonadState WaypointRec)
-
-instance Turtle Waypoint where
-    goNorth        x = modify (\m@WaypointRec{..} -> m { waypointY = waypointY + x })
-    goEast         x = modify (\m@WaypointRec{..} -> m { waypointX = waypointX + x })
-    turnLeft       x = modify (go (x `mod` 360))
-        where go x' m@WaypointRec{..}
-                | x' == 90  = m { waypointX = -waypointY, waypointY = waypointX }
-                | x' == 180 = m { waypointX = -waypointX, waypointY = -waypointY }
-                | x' == 270 = m { waypointX =  waypointY, waypointY = -waypointX }
-                | otherwise = m
-    moveForward    x = modify (\m@WaypointRec{..} -> m 
-                                { waypointEast = waypointEast + x * waypointX
-                                , waypointNorth = waypointNorth + x * waypointY })
-    getManhattanDistance = gets go
-        where go WaypointRec{..} = abs waypointEast + abs waypointNorth
-
 readInput :: (HasLogFunc env, MonadReader env m, MonadIO m, Turtle t) => m [t ()]
 readInput = do
     program <- parse (singleInstruction `sepEndBy1` eol) "data/day12.txt"
                 <$> readFileUtf8 "data/day12.txt"
     either (\e -> do { logError $ display e; return [] })
            return program
+
+data ManhattanRec = ManhattanRec
+    { _mdDirection :: Int
+    , _mdEastWest :: Int
+    , _mdNorthSouth :: Int }
+
+makeLenses ''ManhattanRec
+
+newtype ManhattanDistance a = ManhattanDistance
+    { runMD :: State ManhattanRec a }
+    deriving (Functor, Applicative, Monad, MonadState ManhattanRec)
+
+instance Turtle ManhattanDistance where
+    goNorth = (mdNorthSouth +=)
+    goEast = (mdEastWest +=)
+    turnLeft x = mdDirection %= \d -> (d + x) `mod` 360
+    moveForward x = use mdDirection >>= go
+            where go dir | dir == 0   = goEast x
+                         | dir == 90  = goNorth x
+                         | dir == 180 = goWest x
+                         | dir == 270 = goSouth x
+                         | otherwise  = return ()
+    getManhattanDistance = gets go
+        where go m = abs (m ^. mdEastWest) + abs (m ^. mdNorthSouth)
+
+data WaypointRec = WaypointRec
+    { _waypointX :: Int
+    , _waypointY :: Int
+    , _waypointEast :: Int
+    , _waypointNorth :: Int }
+
+makeLenses ''WaypointRec
+
+newtype Waypoint a = Waypoint
+    { runWaypoint :: State WaypointRec a }
+    deriving (Functor, Applicative, Monad, MonadState WaypointRec)
+
+instance Turtle Waypoint where
+    goNorth = (waypointY +=)
+    goEast = (waypointX +=)
+    turnLeft x = modify (go (x `mod` 360))
+        where go x' m
+                | x' == 90  = m & waypointX .~ -(m ^. waypointY)
+                                & waypointY .~  (m ^. waypointX)
+                | x' == 180 = m & waypointX %~ negate
+                                & waypointY %~ negate
+                | x' == 270 = m & waypointX .~  (m ^. waypointY)
+                                & waypointY .~ -(m ^. waypointX)
+                | otherwise = m
+    moveForward x = modify go
+        where go m = m & waypointEast  +~ (x * (m ^. waypointX))
+                       & waypointNorth +~ (x * (m ^. waypointY))
+    getManhattanDistance = gets go
+        where go m = abs (m ^. waypointEast) + abs (m ^. waypointNorth)
 
 runA :: (HasLogFunc env) => RIO env ()
 runA = do
